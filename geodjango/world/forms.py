782ac9733de461e74e7210845398952ac3c47474
Tanner Olason <tanner.olason@gmail.com>
2021-09-28 23:04:05 -0500

    Update forms.py
    Form things, cool map widget

--- geodjango/world/forms.py
+++ geodjango/world/forms.py
@@ -1,1 +1,9 @@
-hi
+from django.contrib.gis import forms
+from django.contrib.gis.db.models import PointField
+from . import models
+
+class ThingForm(forms.ModelForm):
+    geom = forms.PointField(widget= forms.OSMWidget(attrs={'map_width': 800, 'map_height': 500}) )
+    class Meta:
+        model = models.Things
+        fields = ['title','description','geom']
