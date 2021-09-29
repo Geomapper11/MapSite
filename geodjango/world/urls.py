a987fb8688efdb8f86168b3b8b6d6720aa1a332b
Tanner Olason <tanner.olason@gmail.com>
2021-09-28 23:05:38 -0500

    Update urls.py
    urls

--- geodjango/world/urls.py
+++ geodjango/world/urls.py
@@ -1,1 +1,9 @@
-www and whatnot
+from django.urls import path
+from django.views.generic import ListView
+from . import views
+
+app_name = 'world'
+
+urlpatterns = [
+    path('map/', views.insert_thing, name="insert_thing"),
+]
