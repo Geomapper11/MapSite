3e60376137f40e5b66d79a530b8c4bffb059b7a3
Tanner Olason <tanner.olason@gmail.com>
2021-09-28 23:06:41 -0500

    Update views.py
    saves POST info

--- geodjango/world/views.py
+++ geodjango/world/views.py
@@ -1,1 +1,16 @@
-yup
+from django.urls import path
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Things
+from . import forms
+
+def insert_thing(request):
+    if request.method == 'POST':
+        form = forms.ThingForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            return render(request, 'world/addthing.html', {'form':form})
+    else:
+        form = forms.ThingForm()
+    return render(request, 'world/addthing.html', {'form':form})
